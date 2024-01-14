2bb7be2fe8057ee4e51ca2394f28aad638bbc61f
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2024-01-14 23:28:36 +0500

    Day 11

---
+++ Module 2 - "Strings, Loops & Lists"/Day 11/ReverseString.py
@@ -0,0 +1,11 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Take input from the user
+user_input = input("Enter a string: ")
+
+# Reverse the string
+result = reverse_string(user_input)
+
+# Print the reversed string
+print("Reversed String:", result)
